ec6913d7d582ebe7ca66c05889380fb28aa2803f
Nick Nance <nance.nick@gmail.com>
2014-07-15 06:51:39 -0500

    added fastclick

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ var app = {
 };
 
 $(document).on("ready", function () {
+    new FastClick(document.body);
+
     app.router = new app.routers.AppRouter();
     app.utils.templates.load(["HomeView", "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
         function () {
